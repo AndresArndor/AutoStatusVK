31dfda771c90e62750a6fbc0c71bdde08386e7dc
AndresArndor <naiperlit347@gmail.com>
2019-12-17 16:38:01 +0400

    Update autostatus.py

--- autostatus.py
+++ autostatus.py
@@ -59,8 +59,9 @@ def startStatus():
     else:
         greeting = "Доброго времени суток"
     
-    statusSave = ("{0} {1}! Сейчас {2}℃, облачность {3}%, ветер".format(greeting,
-        city, str(data["main"]["temp"]), str(data["clouds"]["all"]), ))
+    statusSave = ("{0} {1}! Сейчас здесь {2}℃, облачность {3}%, ветер на {4} градусов, давление {5} гектопаскалей, влажность {6}%".format(greeting,
+        city, str(data["main"]["temp"]), str(data["clouds"]["all"]), str(data["wind"]["deg"]), str(data["main"]["pressure"]),
+              str(data["main"]["humidity"])))
     statusOut = requests.get(f"https://api.vk.com/method/status.set?text={statusSave}&v=5.95&access_token={token}").json()
 #    if statusOut.get("error", None):
 #        print(f"Не удалось обновить статус сервер вернул неверный код ответа: {statusOut}")
