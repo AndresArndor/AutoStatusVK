1f428da4d1a62993d13556750678412fd805ccdf
AndresArndor <naiperlit347@gmail.com>
2019-12-17 18:27:07 +0400

    Update autostatus.py

--- autostatus.py
+++ autostatus.py
@@ -42,12 +42,6 @@ def startStatus():
 #        print("У профиля отсутсвует аватар или лайки.")
 #        getLikes = 0
 
-    getValuts = requests.get("https://currate.ru/api/?get=rates&pairs=USDRUB,EURRUB&key=6780a6de85b0690a6e0f02e6fc5bfd4f").json().get("data")
-    Dollar = getValuts.get("USDRUB")
-    Euro = getValuts.get("EURRUB")
-    Dollar = Dollar[:Dollar.find('.')]
-    Euro = Euro[:Euro.find('.')]
-
     today = datetime.datetime.today()
     nowTime = today.strftime("%H")
     nowTime = int(nowTime)
